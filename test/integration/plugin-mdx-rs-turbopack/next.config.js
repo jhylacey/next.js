b9659a63f71b492cdc37a0db1701261e11294df2
OJ Kwon <1210596+kwonoj@users.noreply.github.com>
2023-04-03 19:26:28 -0700

    test(turbopack): add test cases for turbopack-mdx-rs

---
+++ test/integration/plugin-mdx-rs-turbopack/next.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'md'],
+  experimental: {
+    mdxRs: true,
+  },
+}
---
+++ test/integration/plugin-mdx-rs-turbopack/pages/component-md.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Test from './test.md'
+
+export default function Home() {
+  return (
+    <div>
+      <Test />
+    </div>
+  )
+}
